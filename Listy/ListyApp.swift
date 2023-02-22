93c6de940893e7e2edb7d6121223296b6f3bc199
md0r <mihaidorhan@gmail.com>
2023-02-22 15:54:32 +0200

    Initial Commit

---
+++ Listy/ListyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListyApp.swift
+//  Listy
+//
+//  Created by Mihai Dorhan on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
